0e8f04f806303fe8806372dc96cf2ad24b08ad13
Kyung Jin Cho <kj_cho@idealidea.co.kr>
2019-06-22 12:33:21 +0900

    사람을 객체화해서 역할에 달린 기능 수행

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -1,11 +1,29 @@
 package code;
 
+import code.datas.Human;
+
 public class MainDrive {
 
 //	조경진의 브런치 입니다.
 	
 	public static void main(String[] args) {
 		
+		Human h1 = new Human();
+		
+		h1.move(100, 200);
+		
+		h1.defenseCountry();
+		
+		h1.fireGun();
+		
+		h1.payTax(500);
+		
 	}
 	
 }
+
+
+
+
+
+
