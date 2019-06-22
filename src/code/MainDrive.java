a58691c96734c7ef579727852ccd8d7edcd464f2
changseon01 <changseon01@users.noreply.github.com>
2019-06-22 12:33:19 +0900

    사람을 객체화해서 역할을 수행

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -1,12 +1,22 @@
 package code;
 
+import code.datas.Human;
+
 public class MainDrive {
 
 //	브런치 확인용
 	
 	public static void main(String[] args) {
 		
+		Human h1= new Human();
 		
+		h1.move(100, 200);
+		
+		h1.defenseCountry();
+		
+		h1.firegun();
+		
+		h1.paytax(500);
 		
 	}
 	
--- src/code/datas/Animal.java
+++ src/code/datas/Animal.java
@@ -7,7 +7,8 @@ public abstract class Animal {
 	public void move(int x, int y) {
 		
 		String msg = String.format("%d, %d로 이동합니다", x, y);
-		System.out.println("msg");
+		System.out.println(msg);
 	}
+	
 	
 }
