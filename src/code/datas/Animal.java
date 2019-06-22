0ee21ac944a12455f6bdaa8bb6149eac1af5ee50
Kyung Jin Cho <kj_cho@idealidea.co.kr>
2019-06-22 12:19:07 +0900

    기초 클래스, 상속 관계 설정

---
+++ src/code/datas/Animal.java
@@ -0,0 +1,23 @@
+package code.datas;
+
+public abstract class Animal {
+
+	private String name;
+	
+	public void move(int x, int y) {
+		String msg = String.format("%d, %d로 이동합니다.", x, y);
+		System.out.println(msg);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+}
