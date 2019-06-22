95bd588b42a1e1efaa0a480f19b9a2c874a9f0c8
gomsu0 <sm0258@naver.com>
2019-06-22 12:29:18 +0900

    납세자와 군인을
    //놓침

---
+++ src/datas/Army.java
@@ -0,0 +1,7 @@
+package datas;
+
+public class Army {
+
+	public abstract void fireGun();
+	public abstract void defen;
+}
---
+++ src/datas/TexPayer.java
@@ -0,0 +1,7 @@
+package datas;
+
+public interface TexPayer {
+
+	public abstract void payTax(int money);
+		
+}
