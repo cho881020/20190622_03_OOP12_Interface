5af6be0e4d823c71cad93dced0a52fed36a52de8
hanassi <kimmarybbo@gmail.com>
2019-06-22 12:33:32 +0900

    사람에게 역할을 부여함

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -1,11 +1,20 @@
 package code;
 
+import code.datas.Human;
+
 public class MainDrive {
 
 //	김지안 브런치 입니다.
 	
 	public static void main(String[] args) {
-		System.out.println("메롱");
+		
+		Human h1 = new Human();
+		h1.move(100, 200);
+		
+		h1.defenseCountry();
+		h1.fireGun();
+		h1.payTax(50000);
+		
 	}
 	
 }
