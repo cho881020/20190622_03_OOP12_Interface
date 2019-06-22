088351c34a8f6ec7c5c979b1a37a87a8394b91d5
gomsu0 <sm0258@naver.com>
2019-06-22 12:37:07 +0900

    메인드라이브

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -1,9 +1,29 @@
 package code;
 
+import code.datas.Human;
+
 public class MainDrive {
-//sumin�귱ġ
+
+//	
+	
 	public static void main(String[] args) {
+		
+		Human h1 = new Human();
+		
+		h1.move(100, 200);
+		
+		h1.defenseCountry();
+		
+		h1.fireGun();
+		
+		h1.payTax(500);
 		
 	}
 	
 }
+
+
+
+
+
+
